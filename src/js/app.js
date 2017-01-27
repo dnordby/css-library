e79647f068995bcaeb0fda62ca94713efc2c1cdf
Daniel Nordby <nordbydaniel@gmail.com>
2017-01-27 17:12:08 -0500

    misc add files

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// REQUIRE ALL SCSS FROM APP.SCSS
+require("./../scss/app.scss");
+
+// REQUIRE EACH JS MODULE
+require("./home.js");
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+let extractSCSS = new ExtractTextPlugin('./vpv-base.scss');
+module.exports = {
+    entry: "./src/js/app.js",
+    output: {
+        path: 'scss/',
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            {
+              test: /app\.scss$/,
+              loader: extractSCSS.extract(['css', 'sass'])
+            }
+        ]
+    },
+    plugins: [
+      extractSCSS
+    ]
+};
