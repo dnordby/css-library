bc47a3fd5cec7c299a459f12f08ef645e1c96c77
Daniel Nordby <nordbydaniel@gmail.com>
2017-03-17 15:15:14 -0400

    add collapse.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 // REQUIRE ALL SCSS FROM APP.SCSS
 require("./../scss/app.scss");
 
+// REQUIRE COLLAPSE
+require("./collapse.js");
+
 // REQUIRE EACH JS MODULE
 require("./home.js");
--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,4 @@
 function exportHome() {
-  console.log("It works from home.js.");
 }
 
 module.exports = exportHome();
